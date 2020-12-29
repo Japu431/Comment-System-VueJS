0595b924d6d1d7297f0345a65b7eb3da4ce7136f
Yusuke-kun <japamnk@gmail.com>
2020-12-29 16:46:06 -0300

    Projeto Completo

---
+++ vuejs-cli/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <TemplateComents />
+</template>
+
+<script>
+import Comments from "./components/Comment";
+
+export default {
+  name: "app",
+  components: {
+    TemplateComents: Comments
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vuejs-cli/src/components/Form.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="form-todo form-group">
+    <p>
+      <input
+        type="text"
+        name="author"
+        id="author"
+        placeholder="Nome"
+        class="form-control"
+        v-model="name"
+      />
+    </p>
+    <p>
+      <textarea
+        type="text"
+        name="message"
+        id="message"
+        placeholder="Comentário..."
+        class="form-control"
+        v-model="message"
+      ></textarea>
+    </p>
+    <p>
+      <button v-on:click="addComment" type="submit" class="btn btn-primary">
+        Comentar
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      message: "",
+    };
+  },
+  methods: {
+    addComment() {
+      if (this.message.trim() === "") {
+        return;
+      }
+
+      this.$emit("add-todo", {
+        name: this.name,
+        message: this.message,
+      }),
+        (this.name = ""),
+        (this.message = "");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
